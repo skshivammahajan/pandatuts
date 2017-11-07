bf05eee366dfa386ffced723a3c389b7f6eb3110
Shivam Mahajan <skshivammahajan@gmail.com>
2017-11-07 13:38:46 +0000

    Reading and writing in Excel

--- pandaexcel.py
+++ pandaexcel.py
@@ -12,3 +12,56 @@ df.to_excel(writer, sheet_name='Sheet1')
 
 # Close the Pandas Excel writer and output the Excel file.
 writer.save()
+
+# Reading Excel# load the sheet into the Pandas Dataframe structure
+df = pd.read_excel('Pandas-Example.xlsx', sheetname='Sheet1')
+
+print("The list of row indicies")
+print(df.index)
+print("The column headings")
+print(df.columns)
+
+print("The 'Patient' column information:")
+print(df['Patient'])
+
+# print each row of the patient column
+for i in df.index:
+    print(df['Patient'][i])
+
+# compute a new column as the product of two other columns
+for i in df.index:
+    df['BP*SO2'][i] = df['BP'][i] * df['SO2'][i]
+
+print("results of column multiplication:")
+print(df['BP*SO2'])
+
+# write the dataframe back out with the new column data included
+writer = ExcelWriter('Pandas-Example-Out.xlsx')
+df.to_excel(writer,'Sheet1',index=False)
+writer.save()
+# load the sheet into the Pandas Dataframe structure
+df = pd.read_excel('Pandas-Example.xlsx', sheetname='Sheet1')
+
+print("The list of row indicies")
+print(df.index)
+print("The column headings")
+print(df.columns)
+
+print("The 'Patient' column information:")
+print(df['Patient'])
+
+# print each row of the patient column
+for i in df.index:
+    print(df['Patient'][i])
+
+# compute a new column as the product of two other columns
+for i in df.index:
+    df['BP*SO2'][i] = df['BP'][i] * df['SO2'][i]
+
+print("results of column multiplication:")
+print(df['BP*SO2'])
+
+# write the dataframe back out with the new column data included
+writer = ExcelWriter('Pandas-Example-Out.xlsx')
+df.to_excel(writer,'Sheet1',index=False)
+writer.save()
